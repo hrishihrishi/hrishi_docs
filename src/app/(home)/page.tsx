38168e6763a9734b4c30d09b109c38961d1d6727
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-07-26 17:27:39 +0530

    feat(documents): Implement partial CRUD operations

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
+
+import { useSearchParam } from "@/hooks/use-search-param";
 
 import { Navbar } from "./navbar";
+import { DocumentsTable } from "./documents-table";
 import { TemplatesGallery } from "./templates-gallery";
-
 import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
-  const documents = useQuery(api.documents.get);
-
-  if (documents === undefined) {
-    return (
-      <p>Loading...</p>
-    )
-  }
+  const [search] = useSearchParam();
+  const { 
+    results, 
+    status, 
+    loadMore
+  } = usePaginatedQuery(api.documents.get, { search }, { initialNumItems: 5 });
 
   return ( 
     <div className="min-h-screen flex flex-col">
@@ -23,9 +24,11 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
-        {documents?.map((document) => (
-          <span key={document._id}>{document.title}</span>
-        ))}
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
       </div>
     </div>
    );
--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+
 import { cn } from "@/lib/utils";
 import {
   Carousel,
@@ -10,8 +14,23 @@ import {
 } from "@/components/ui/carousel";
 import { templates } from "@/constants/templates";
 
+import { api } from "../../../convex/_generated/api";
+
 export const TemplatesGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+      .then((documentId) => {
+        router.push(`/documents/${documentId}`);
+      })
+      .finally(() => {
+        setIsCreating(false);
+      });
+  };
 
   return (
     <div className="bg-[#F1F3F4]">
@@ -32,7 +51,8 @@ export const TemplatesGallery = () => {
                 >
                   <button
                     disabled={isCreating}
-                    onClick={() => {}}
+                    // TODO: Add proper initial content
+                    onClick={() => onTemplateClick(template.label, "")}
                     style={{
                       backgroundImage: `url(${template.imageUrl})`,
                       backgroundSize: "cover",
