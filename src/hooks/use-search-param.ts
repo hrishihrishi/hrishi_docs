e8d0c9ff09f8f532badf29fdeba7a6f4d74fceee
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-07-28 11:20:04 +0530

    fix(collab): add room and organization permissions

--- src/hooks/use-search-param.ts
+++
@@ -1,8 +0,0 @@
-import { parseAsString, useQueryState } from "nuqs";
-
-export function useSearchParam() {
-  return useQueryState(
-    "search",
-    parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
-  );
-};
